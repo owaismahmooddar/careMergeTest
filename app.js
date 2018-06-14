34c438d8153ebce0a618c88efdd72d202370b196
owaismahmooddar <40254324+owaismahmooddar@users.noreply.github.com>
2018-06-13 19:49:17 -0700

    Add files via upload
    To run the app enter the following two commands:
    npm install
    npm run serve

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const methods = require('./methods')
+const opn = require("opn");
+const app = express();
+
+app.get('/i/want/title', methods.processRequest,);
+
+app.get('*', function (req, res) {
+    res.status(404).send('Sorry, Looks like a 404 !');
+});
+
+module.exports = {
+    start: (port) => {
+        app.listen(port, function () {
+            console.log('Example app listening on port - ' + port);
+            opn('http://localhost:' + port + '/i/want/title?address=http://www.google.com',{app: 'chrome'} , function (err) {
+                if (err) throw err;
+                console.log('The user closed the browser');
+            });
+        });
+    }
+}
